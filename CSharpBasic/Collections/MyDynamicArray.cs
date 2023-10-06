9d408dd27f548dd54375884a9a7b4faad12b3ca2
dollbride <polly.dollbride@gmail.com>
2023-10-06 17:00:35 +0900

    Blue Potions in Inventory

---
+++ CSharpBasic/Collections/MyDynamicArray.cs
@@ -0,0 +1,91 @@
+﻿namespace Collections
+{
+    internal class MyDynamicArray
+    {
+        public object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+                _items[index] = value;
+            }
+        }
+        public int Count => _count;
+        public int Capacity => _items.Length;
+
+        private int _count;
+        private const int DEFAULT_SIZE = 1;
+        private object[] _items = new object[DEFAULT_SIZE];
+
+        public void Add(object item)
+        {
+            if (_count >= _items.Length)
+            {
+                object[] tmp = new object[_count * 2];
+                Array.Copy(_items, tmp, _count);
+                _items = tmp;
+            }
+            _items[_count++] = item;
+        }
+        
+        public object Find(Predicate<object> match)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(_items[i]))
+                    return _items[i];
+            }
+            return default;
+        }
+       
+        public int FindIndex(Predicate<object> match)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(_items[i]))
+                    return i;
+            }
+            return -1;
+        }
+
+        public bool Contains(object item)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (_items[i] == item)
+                    return true;
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new IndexOutOfRangeException();
+
+            for (int i = index; i < _count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _count--;
+        }
+
+        public bool Remove(object item)
+        {
+            int index = FindIndex(x => x == item);
+
+            if (index < 0)
+                return false;
+
+            RemoveAt(index);
+            return true;
+        }
+    }
+}
---
+++ CSharpBasic/Collections/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Collections
+{
+    enum ItemID
+    {
+        RedPotion = 20, BluePotion = 21
+    }
+    class SlotData
+    {
+        public bool isEmpty => id == 0 && num == 0;
+        public int id;
+        public int num;
+
+        public SlotData(int id, int num)
+        {
+            this.id = id;
+            this.num = num;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDynamicArray inventory = new MyDynamicArray();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                inventory.Add(new SlotData(0, 0));
+                
+            }
+
+            inventory[0] = new SlotData((int)ItemID.RedPotion, 40);
+            inventory[1] = new SlotData((int)ItemID.BluePotion, 99);
+            inventory[2] = new SlotData((int)ItemID.BluePotion, 50);
+
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                Console.WriteLine($"슬롯 {i} : [{(ItemID)((SlotData)inventory[i]).id}], [{((SlotData)inventory[i]).num}]");
+            }
+            Console.WriteLine("\n파란포션 5개 습득 후 인벤토리 정보:\n");
+            //파란 포션 5개 획득
+            //1. 파란 포션 5개 들어갈 수 있는 슬롯 찾기
+            int availableSlotIndex = inventory.FindIndex(slotData => ((SlotData)slotData).isEmpty ||
+                  (((SlotData)slotData).id == (int)ItemID.BluePotion && ((SlotData)slotData).num <= 99 - 5));
+
+            //2. 해당 슬롯의 아이템 개수에다 내가 추가하려는 개수를 더한 만큼의 수정 예상 값(int expected)을 구함
+            int expected = ((SlotData)inventory[availableSlotIndex]).num + 5;
+
+            //3. 새로운 아이템 데이터를 만들어서 슬롯 데이터를 교체
+            SlotData newSlotData = new SlotData((int)ItemID.BluePotion, expected);
+            inventory[availableSlotIndex] = newSlotData;
+
+            for (int i =0; i < inventory.Count; i++)
+            {
+                Console.WriteLine($"슬롯 {i} : [{(ItemID)((SlotData)inventory[i]).id}], [{((SlotData)inventory[i]).num}]");
+            }
+        }
+    }
+}
